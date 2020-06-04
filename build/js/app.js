ed7ed304f1a400b3117231575d4a97c085d223f1
Puchok-93 <paul.puchkov@yandex.ru>
2020-06-04 19:11:25 +0300

    Завершение проекта

--- build/js/app.js
+++ build/js/app.js
@@ -1,32 +1,78 @@
 'use strict';
+
+var callbackPopup = document.querySelector('.callback-popup');
+var callbackPopupOverlay = document.querySelector('.callback-popup-overlay');
+var callbackButton = document.querySelector('.callback-button');
+var closePopupButton = callbackPopup.querySelector('.callback-popup__button-close');
+var form = document.querySelector('.callback-popup-form');
+var userName = form.querySelector('[name=user-name]');
+var userPhone = form.querySelector('[name=user-phone]');
+var userQuestion = form.querySelector('[name=user-question]');
+var aboutCompanyBlock = document.querySelector('.about-company');
+var companyDescription = aboutCompanyBlock.querySelector('.about-company__description--hired');
+
 var sectionsWebsiteList = document.querySelector('.sections-website-list');
 var sectionsWebsiteButtonList = document.querySelector('.sections-website__button');
-
 var ourOfficeList = document.querySelector('.our-office-list');
 var ourOfficeListButton = document.querySelector('.our-office__button');
 
+var ESC_KEY = 27;
 
-var test = function () {
-  var mainBlockButton = document.querySelector('.main-block-info__link');
-  var tabletWidth = 768;
+if (window.matchMedia('screen and (min-width: 767px)').matches) {
+  sectionsWebsiteList.classList.add('hide');
+  ourOfficeList.classList.add('hide');
+}
 
-  if (window.outerWidth > tabletWidth) {
-    mainBlockButton.textContent = 'Получить бесплатную консультацию';
-  } else {
-    mainBlockButton.textContent = 'Бесплатная консультация';
-  }
-
+var setDefaultValues = function () {
+  userName.style.border = 'none';
+  userPhone.style.border = 'none';
+  userQuestion.style.border = 'none';
 };
 
+var openPopup = function () {
+  callbackPopupOverlay.classList.add('callback-popup-overlay--show');
+  callbackPopup.classList.add('callback-popup--show');
+  userName.focus();
+};
+
+var closePopup = function () {
+  callbackPopupOverlay.classList.remove('callback-popup-overlay--show');
+  callbackPopup.classList.remove('callback-popup--show');
+  setDefaultValues();
+};
+
+form.addEventListener('submit', function () {
+  localStorage.setItem('name', userName.value);
+  localStorage.setItem('phone', userPhone.value);
+  localStorage.setItem('question', userQuestion.value);
+});
+
+var reductionDescription = function () {
+  if (window.matchMedia('screen and (min-width: 1023px)').matches) {
+    companyDescription.textContent = 'Наши поставщики - мировые производители электронных компонентов: OSRAM, CREE, HOLGLITRONIC, REFOND. ' +
+    'Печатные платы и комплектующие Service Devices применяются на предприятиях Российских Железных Дорог..';
+  }
+};
 
 var openSectionsWebsiteList = function () {
   sectionsWebsiteList.classList.toggle('hide');
+  sectionsWebsiteButtonList.classList.toggle('sections-website__button--open');
 };
 
 var openOurOfficeList = function () {
   ourOfficeList.classList.toggle('hide');
+  ourOfficeListButton.classList.toggle('our-office__button--open');
 };
 
 sectionsWebsiteButtonList.addEventListener('click', openSectionsWebsiteList);
 ourOfficeListButton.addEventListener('click', openOurOfficeList);
-window.addEventListener('resize', test);
+window.addEventListener('resize', reductionDescription);
+callbackButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+callbackPopupOverlay.addEventListener('click', closePopup);
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESC_KEY) {
+    evt.preventDefault();
+    closePopup();
+  }
+});
