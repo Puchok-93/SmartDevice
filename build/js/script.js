f7de0a79b48f0c94763e04349af1cc300674a1d5
Puchok-93 <paul.puchkov@yandex.ru>
2020-06-10 20:09:25 +0300

    Исправление багов 10.06

--- build/js/script.js
+++ build/js/script.js
@@ -1,13 +1,17 @@
 'use strict';
-/*
+
+var body = document.querySelector('body');
 var callbackPopup = document.querySelector('.callback-popup');
+var openCallbackButton = document.querySelector('.callback-button');
+var closePopupButton = document.querySelector('.callback-popup__button-close');
 var callbackPopupOverlay = document.querySelector('.callback-popup-overlay');
-var callbackButton = document.querySelector('.callback-button');
-var closePopupButton = callbackPopup.querySelector('.callback-popup__button-close');
+var wrapper = document.querySelector('.wrapper');
+
 var form = document.querySelector('.callback-popup-form');
 var userName = form.querySelector('[name=user-name]');
 var userPhone = form.querySelector('[name=user-phone]');
 var userQuestion = form.querySelector('[name=user-question]');
+
 var aboutCompanyBlock = document.querySelector('.about-company');
 var companyDescription = aboutCompanyBlock.querySelector('.about-company__description--hired');
 
@@ -16,85 +20,43 @@ var sectionsWebsiteButtonList = document.querySelector('.sections-website__butto
 var ourOfficeList = document.querySelector('.our-office-list');
 var ourOfficeListButton = document.querySelector('.our-office__button');
 
+var ESC_KEY = 27;
+
 sectionsWebsiteList.classList.add('hide');
 ourOfficeList.classList.add('hide');
 sectionsWebsiteButtonList.classList.add('sections-website__button--closed');
 ourOfficeListButton.classList.add('our-office__button--closed');
 
-userPhone.addEventListener('focus', function (evt) {
-  evt.preventDefault();
-  userPhone.value = '+7(';
-
-  if (userPhone.length === 6) {
-    userPhone.value.style.color = 'red';
-  }
-});
-
-var ESC_KEY = 27;
-
-/*-----------------------------------------*/
-const body = document.querySelector('body')
-const modal = document.querySelector('.callback-popup')
-const openModalButton = document.querySelector('.callback-button')
-const closeModalButton = document.querySelector('.callback-popup__button-close')
-
-function existVerticalScroll() {
-   return document.body.offsetHeight > window.innerHeight
-}
-
-function getBodyScrollTop() {
-    return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
-  }
-
-openModalButton.addEventListener('click', e => {
-  e.preventDefault()
-
-  body.dataset.scrollY = getBodyScrollTop() // сохраним значение скролла
-
-
-  modal.classList.add('callback-popup--show')
-
-  if(existVerticalScroll()) {
-     body.classList.add('body-lock')
-     body.style.top = `-${body.dataset.scrollY}px`
-   }
-})
-
-closeModalButton.addEventListener('click', e => {
-  e.preventDefault()
-
-  modal.classList.remove('callback-popup--show')
-
-  if(existVerticalScroll()) {
-   body.classList.remove('body-lock')
-   window.scrollTo(0,body.dataset.scrollY)
-  }
-})
-
-/*-----------------------------------------
 var setDefaultValues = function () {
+  userName.value = '';
+  userPhone.value = '';
+  userQuestion.value = '';
   userName.style.border = 'none';
   userPhone.style.border = 'none';
   userQuestion.style.border = 'none';
-};*/
-/*
-var openPopup = function () {
+};
+
+var getBodyScrollTop = function () {
+  return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
+}
+
+var openPopup = function (evt) {
+  evt.preventDefault();
+  body.dataset.scrollY = getBodyScrollTop();
+  body.style.top = `-${body.dataset.scrollY}px`;
   callbackPopupOverlay.classList.add('callback-popup-overlay--show');
   callbackPopup.classList.add('callback-popup--show');
+  body.classList.add('body-lock');
   userName.focus();
 };
 
-var closePopup = function () {
+var closePopup = function (evt) {
   callbackPopupOverlay.classList.remove('callback-popup-overlay--show');
   callbackPopup.classList.remove('callback-popup--show');
+  body.classList.remove('body-lock')
+  window.scrollTo(0,body.dataset.scrollY)
   setDefaultValues();
 };
-
-form.addEventListener('submit', function () {
-  localStorage.setItem('name', userName.value);
-  localStorage.setItem('phone', userPhone.value);
-  localStorage.setItem('question', userQuestion.value);
-});
 
 var reductionDescription = function () {
   if (window.matchMedia('screen and (min-width: 1023px)').matches) {
@@ -117,12 +79,15 @@ var openOurOfficeList = function () {
   sectionsWebsiteButtonList.classList.add('sections-website__button--closed');
 };
 
-sectionsWebsiteButtonList.addEventListener('click', openSectionsWebsiteList);
-ourOfficeListButton.addEventListener('click', openOurOfficeList);
+form.addEventListener('submit', function () {
+  localStorage.setItem('name', userName.value);
+  localStorage.setItem('phone', userPhone.value);
+  localStorage.setItem('question', userQuestion.value);
+});
 
+openCallbackButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 window.addEventListener('resize', reductionDescription);
-//callbackButton.addEventListener('click', openPopup);
-//closePopupButton.addEventListener('click', closePopup);
 callbackPopupOverlay.addEventListener('click', closePopup);
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ESC_KEY) {
@@ -130,4 +95,9 @@ window.addEventListener('keydown', function (evt) {
     closePopup();
   }
 });
-*/
+
+document.getElementById('phone').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+
